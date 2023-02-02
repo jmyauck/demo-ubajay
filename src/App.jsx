36d0649f39c4369a5d00934f00f9f53d143e5f6e
jmyauck <yauck.jm@gmail.com>
2023-02-02 15:25:01 -0300

    primera version

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+     Hola
+    </div>
+  )
+}
+
+export default App;
